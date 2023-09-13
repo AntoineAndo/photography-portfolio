fe5a3b61a84f23b4c710d488a0b738755b909052
Antoine Ando <antoine.ando@gmail.com>
2023-09-13 16:50:12 +0200

    Added prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
 import vercelStatic from "@astrojs/vercel/static";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "bgp3zmck",
-      dataset: "production",
-      apiVersion: "2023-02-08",
-    }),
-    react(),
-  ],
+  integrations: [sanity({
+    projectId: "bgp3zmck",
+    dataset: "production",
+    apiVersion: "2023-02-08"
+  }), react(), prefetch()],
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercelStatic()
 });
