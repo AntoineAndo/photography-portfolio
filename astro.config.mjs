1b04907154cd82930891f2f888366aef9413704d
Antoine Ando <antoine.ando@gmail.com>
2023-09-13 16:09:13 +0200

    Vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
     projectId: "bgp3zmck",
     dataset: "production",
     apiVersion: "2023-02-08"
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: vercel()
 });
