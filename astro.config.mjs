ee7dd9c0e411a7c9ac4ca89506592a2cee49f6c6
Antoine Ando <antoine.ando@gmail.com>
2023-09-13 16:38:23 +0200

    Fix style path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: "bgp3zmck",
-    dataset: "production",
-    apiVersion: "2023-02-08"
-  }), react()],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    sanity({
+      projectId: "bgp3zmck",
+      dataset: "production",
+      apiVersion: "2023-02-08",
+    }),
+    react(),
+  ],
+  output: "static",
+  adapter: vercelStatic(),
 });
