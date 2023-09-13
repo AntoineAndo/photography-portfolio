93035bbf56ffe5ea5560483ee140a9c8cd644822
Antoine Ando <antoine.ando@gmail.com>
2023-09-13 15:57:48 +0200

    v0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "bgp3zmck",
-      dataset: "production",
-      apiVersion: "2023-02-08",
-    }),
-  ],
+  integrations: [sanity({
+    projectId: "bgp3zmck",
+    dataset: "production",
+    apiVersion: "2023-02-08"
+  }), react()]
 });
